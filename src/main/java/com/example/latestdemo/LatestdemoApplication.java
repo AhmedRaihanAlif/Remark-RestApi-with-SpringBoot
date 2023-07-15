083c8730667d2b37a7a66b19462dba24272f43b6
Ahmed Raihan <ahmedraihanalif@gmail.com>
2023-07-15 16:54:30 +0600

    first commit

---
+++ src/main/java/com/example/latestdemo/LatestdemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.latestdemo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class LatestdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatestdemoApplication.class, args);
+	}
+
+
+}
+
---
+++ src/main/java/com/example/latestdemo/student/StudentConfig.java
@@ -0,0 +1,27 @@
+package com.example.latestdemo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.AUGUST;
+
+@Configuration
+public class StudentConfig {
+
+@Bean
+    CommandLineRunner commandLineRunner (StudentRepository repository){
+        return args -> {
+              Student ahmedRaihanAlif=  new Student("Ahmed Raihan Alif","alif@gmail.com", LocalDate.of(2000, AUGUST,25),25);
+              Student asifRaihan=  new Student("Asif Raihan  ","asif@gmail.com", LocalDate.of(1998, AUGUST,25),25);
+
+              repository.saveAll(
+                       List.of(ahmedRaihanAlif, asifRaihan)
+              );
+        };
+    }
+}
